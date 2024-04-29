b68b7d8fe3ec7499c36a27708ad254d2dbfcc1fd
LimDae94 <piay801@naver.com>
2024-04-29 16:26:10 +0900

    🎉INIT: 기본 프로젝트 시작
    🎉INIT: 기본 프로젝트 시작

---
+++ aptner/src/main/java/com/fastcampus/aptner/AptnerApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.aptner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptnerApplication.class, args);
+	}
+
+}
