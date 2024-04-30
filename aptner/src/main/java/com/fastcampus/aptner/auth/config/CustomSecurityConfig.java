481b2903bf0e5192e05847f3c3d6161a920fcaf0
LimDae94 <piay801@naver.com>
2024-05-01 00:02:19 +0900

    ✨ENH: 스프링 시큐리티 기본 설정 및 추가하기(#3)
    1. 모든 요청에 대해 허용된 상태
    - 지금은 개발된 코드가 전혀 없는 상태이기 때문에 모든 요청에 허용된 상태로 작성했다.
    
    2. 모든 경로에 CORS 설정
    - "GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"  요청에 대해 허용을 설정한 상태이다.
    - 헤더에 포함된 세 개의 파라미터 의미는 다음과 같다. "Authorization" : 클라이언트가 서버로 보내는 요청에 인증 정보를 포함시키는 데 사용(주로 토큰 기반의 인증에 사용)
    "Cache-Control" : 캐시 제어를 위해 사용되는 헤더(클라이언트 또는 프록시가 응답을 캐싱할 때 캐시 동작을 지정)
    "Content-Type" : HTTP 요청 또는 응답 본문의 컨텐츠 타입을 지정하는 데 사용(application/json, text/html 등)
    참고로, HTTP 표준 헤더이다.
    
    3. 비밀번호 암호화
    BCryptPasswordEncoder 으로 비밀번호 암호화한다.

---
+++ aptner/src/main/java/com/fastcampus/aptner/auth/config/CustomSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.fastcampus.aptner.auth.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Log4j2
+@RequiredArgsConstructor
+@Configuration
+public class CustomSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                authorizationManagerRequestMatcherRegistry.anyRequest().permitAll());
+
+        http.cors(httpSecurityCorsConfigurer ->
+                httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()));
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOriginPatterns(List.of("*")); // 어디서든 허락
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+}
