1561ca1cd84cb5148b23f8e51dc161777b8b5226
LimDae94 <piay801@naver.com>
2024-05-01 22:56:23 +0900

    ♻️REF: 테스트 데이터 수정하기(#16)
    보기 편한 값으로 변경했다.

--- aptner/src/test/java/com/fastcampus/aptner/member/repository/MemberRepositoryTest.java
+++ aptner/src/test/java/com/fastcampus/aptner/member/repository/MemberRepositoryTest.java
@@ -23,21 +23,21 @@ class MemberRepositoryTest {
     public void InsertMember() {
         // given
         for (int i = 0; i < 10; i++) {
-            Member createdMember = Member.builder()
-                    .email("member" + i + "@gmail.com")
+            Member member = Member.builder()
+                    .email("user" + i + "@aaa.com")
                     .password(passwordEncoder.encode("1111"))
                     .nickname("USER" + i)
-                    .content(i + "동 반포자이")
+                    .content("USER content " + i)
                     .phone("010-1234-1234")
                     .socialLogin(false)
                     .build();
 
-            createdMember.addRole(MemberRole.USER);
+            member.addRole(MemberRole.USER);
 
-            if (i > 5) {createdMember.addRole(MemberRole.MANAGER);}
-            if (i > 8) {createdMember.addRole(MemberRole.ADMIN);}
+            if (i > 5) {member.addRole(MemberRole.MANAGER);}
+            if (i > 8) {member.addRole(MemberRole.ADMIN);}
 
-            memberRepository.save(createdMember);
+            memberRepository.save(member);
         }
         // expected
         Assertions.assertThat(memberRepository.count()).isEqualTo(10);
