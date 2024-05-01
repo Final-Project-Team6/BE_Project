a937aac620b0d9cf49617ca7c016182c99fb6de0
LimDae94 <piay801@naver.com>
2024-05-01 22:54:27 +0900

    ♻️REF: 회원 리포지토리 수정하기
    회원 조회는 id 보다는 email 으로 하는 것으로  변경해서 id 를 통해 다시 email 을 반복 조회가 발생되지 않도록 했다.
    
    반환 타입을 Optional 으로 변경해서 해당 메서드를 사용하는 코드에서 쉽게 반환과 예외를 처리할 수 있도록 변경했다.

--- aptner/src/main/java/com/fastcampus/aptner/member/repository/MemberRepository.java
+++ aptner/src/main/java/com/fastcampus/aptner/member/repository/MemberRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @EntityGraph(attributePaths = {"memberRoleList"})
-    @Query("select m from Member m where m.id= :id")
-    Member getWithRoles(@Param("id") Long id);
+    @Query("select m from Member m where m.email= :email")
+    Optional<Member> getWithRoles(@Param("email") String email);
 
 }
