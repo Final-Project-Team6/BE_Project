097dbec715aa71df29e49a6c832d337700b24b99
LimDae94 <piay801@naver.com>
2024-05-07 17:30:57 +0900

    ✨ENH : 예외 처리하기
    - DataDuplicateKeyException
    1. 중복된 값에 대한 예외 처리
    
    - DataNotFoundException
    1. 값이 없는 경우에 대한 예외 처리
    
    - NotMatchPasswordException
    1. 비밀번호가 맞지 않는 경우에 대한 예외 처리
    
    - SamePasswordException
    1. 비밀번호가 동일한 경우에 대한 예외 처리

---
+++ aptner/src/main/java/com/fastcampus/aptner/global/handler/exception/DataNotFoundException.java
@@ -0,0 +1,8 @@
+package com.fastcampus.aptner.global.handler.exception;
+
+public class DataNotFoundException extends NullPointerException {
+
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
