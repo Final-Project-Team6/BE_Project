1c9855872a03ebf0f816df1482f13b76bc8983af
LimDae94 <piay801@naver.com>
2024-05-10 21:25:09 +0900

    ✨ENH: 회원가입요청 DTO 생성하기(#30)
    회원가입 요청에 따른 DTO 클래스를 생성한다.
    
    - @Pattern 으로 필드마다 요구 사항에 따른 정규 표현식 적용하기
    - @Size 는 Validation 에서 제공되고, Bean 유효성 검사를 수행한다.
    
    따라서, @Column을 @Size 와 함께 사용하여 Bean 유효성 검사와 함께 데이터베이스 열 속성을 지정할 수 있다.
    
    Difference Between @Size, @Length, and @Column(length=value) 밸덩
    - https://www.baeldung.com/jpa-size-length-column-differences
    
    @Column,-@Size,-@NotNull 정리한 위키
    - https://github.com/Final-Project-Team6/BE_Project/wiki/@Column,-@Size,-@NotNull-%EC%A4%91%EC%97%90%EC%84%9C-%EC%84%A0%ED%83%9D%EC%9D%80%3F

---
+++ aptner/src/main/java/com/fastcampus/aptner/member/dto/reqeust/SignUpMemberRequest.java
@@ -0,0 +1,69 @@
+package com.fastcampus.aptner.member.dto.reqeust;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class SignUpMemberRequest {
+
+    @NotBlank(message = "동의여부: 서비스 이용약관 동의는 필수입니다.")
+    private boolean termsService;
+
+    @NotBlank(message = "동의여부: 개인정보 수집 동의는 필수입니다.")
+    private boolean privateInformationCollection;
+
+    private boolean snsMarketingInformationReceive; // SNS 마케팅 정보 수신 동의는 선택입니다.
+
+    @NotBlank(message = "이름: 필수 정보입니다.")
+    @Pattern(regexp = "^[a-zA-Z가-힣]{1,16}$", message = "이름: 1자 이상 16자 이하의 이름을 입력해주세요.")
+    @Size(min = 1, max = 16)
+    private String fullName;
+
+    @Pattern(regexp = "^\\d{6}$", message = "주민등록번호: 6자리 숫자를 입력해주세요.")
+    @Size(max = 6)
+    private String birthFirst; // 본인인증없이 가입된 회원은 선택입니다.
+
+    @Pattern(regexp = "^[1-4]$", message = "주민등록번호: 1자리 숫자를 입력해주세요.")
+    @Size(max = 1)
+    private String gender;  // 본인인증없이 가입된 회원은 선택입니다.
+
+    @Pattern(regexp = "^[a-zA-Z가-힣]+$", message = "통신사: 통신사를 선택하세요.")
+    @Size(min = 2, max = 10)
+    private String phoneCarrier; // 본인인증없이 가입된 회원은 선택입니다.
+
+    @NotBlank(message = "휴대전화번호: 필수 정보입니다.")
+    @Pattern(regexp = "^[0-9]{10,11}$", message = "휴대전화번호: 숫자만 입력해주세요.")
+    @Size(max = 11)
+    private String phone;
+
+    @NotBlank(message = "아이디: 필수 정보입니다.")
+    @Pattern(regexp = "^[a-z0-9_-]{5,20}$", message = "아이디: 5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.")
+    @Size(min = 5, max = 20)
+    private String username;
+
+    @NotBlank(message = "비밀번호: 필수 정보입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}"
+            , message = "비밀번호: 8~20자 영문 대소문자, 숫자, 특수문자를 조합하여 작성해야 합니다.")
+    @Size(min = 8, max = 20)
+    private String password;
+
+    @NotBlank(message = "닉네임: 필수 정보입니다.")
+    @Pattern(regexp = "(?=\\S+$)[\\w가-힣a-zA-Z]{2,16}"
+            , message = "닉네임: 공백이나 특수문자를 사용할 수 없으며, 2자 이상 16자 이하여야 합니다.")
+    @Size(min = 2, max = 16)
+    private String nickname;
+
+    @NotBlank(message = "아파트 동(층): 필수 정보입니다.")
+    @Pattern(regexp = "^[a-zA-Z가-힣0-9]{1,8}$"
+            , message = "동(층): 영어 대소문자, 한글, 숫자만 입력 가능하며, 1자 이상 8자 이하여야 합니다.")
+    @Size(min = 1, max = 8)
+    private String dong;
+
+    @NotBlank(message = "아파트 호: 필수 정보입니다.")
+    @Pattern(regexp = "^[a-zA-Z가-힣0-9]{1,8}$"
+            , message = "아파트 호: 영어 대소문자, 한글, 숫자만 입력 가능하며, 1자 이상 8자 이하여야 합니다.")
+    @Size(min = 1, max = 8)
+    private String ho;
+}
