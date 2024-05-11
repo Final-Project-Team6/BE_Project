19e8a1c9dc24bc3947b8474691c4871f06a9d920
cutegyuseok <yigyuseok@gmail.com>
2024-05-12 03:35:52 +0900

    ⚙️SET : erd 변경에 따른 entity 삭제

--- aptner/src/main/java/com/fastcampus/aptner/post/communication/repository/CommunicationCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fastcampus.aptner.post.communication.repository;
-
-import com.fastcampus.aptner.post.communication.domain.CommunicationComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommunicationCommentRepository extends JpaRepository<CommunicationComment,Long> {
-}
