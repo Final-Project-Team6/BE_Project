b25f1d54c9a4e35c5e334ede6e59c81e76167dc5
cutegyuseok <yigyuseok@gmail.com>
2024-05-16 23:36:19 +0900

    📝DOC : Swagger 문서화 작업

--- aptner/src/main/java/com/fastcampus/aptner/post/opinion/domain/CommentType.java
+++ aptner/src/main/java/com/fastcampus/aptner/post/opinion/domain/CommentType.java
@@ -6,10 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CommentType {
-    ANNOUNCEMENT("공지사항 댓글"),
-    COMPLAINT("민원 댓글"),
-    COMMUNICATION("소통 공간 댓글"),
-    REPLY("대댓글");
+    ANNOUNCEMENT("공지사항 댓글"), COMPLAINT("민원 댓글"), COMMUNICATION("소통 공간 댓글"), REPLY("대댓글");
     private final String CommentType;
 
 }
