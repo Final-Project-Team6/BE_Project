cff8c602aa10fc140a77fdd717e08db2f9c53f04
cutegyuseok <yigyuseok@gmail.com>
2024-05-17 00:20:33 +0900

    ✨ENH : Swagger Server Url Setting

--- aptner/src/main/java/com/fastcampus/aptner/global/config/SwaggerConfig.java
+++ aptner/src/main/java/com/fastcampus/aptner/global/config/SwaggerConfig.java
@@ -5,11 +5,14 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -20,8 +23,15 @@ public class SwaggerConfig {
                 .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
                 .in(SecurityScheme.In.HEADER).name("Authorization");
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
-
+        Server server = new Server();
+        server.setUrl("https://aptner.shop");
+        Server local = new Server();
+        local.setUrl("http://localhost:8080");
+        List<Server> serverList = new ArrayList<>();
+        serverList.add(server);
+        serverList.add(local);
         return new OpenAPI()
+                .servers(serverList)
                 .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
                 .security(Arrays.asList(securityRequirement))
                 .info(new Info()
