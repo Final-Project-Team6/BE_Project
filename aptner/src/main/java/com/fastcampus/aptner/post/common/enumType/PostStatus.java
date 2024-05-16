11730f21cfba95fb159d5a469f883baef38a64a4
cutegyuseok <yigyuseok@gmail.com>
2024-05-16 23:40:12 +0900

    📝DOC : enum 가로로 정렬

--- aptner/src/main/java/com/fastcampus/aptner/post/common/enumType/PostStatus.java
+++ aptner/src/main/java/com/fastcampus/aptner/post/common/enumType/PostStatus.java
@@ -6,9 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum PostStatus {
-    PUBLISHED("게시됨"),
-    HIDDEN("숨김"),
-    DELETED("삭제됨"),
-    FORCE_DELETED("강제 삭제됨");
+    PUBLISHED("게시됨"), HIDDEN("숨김"), DELETED("삭제됨"), FORCE_DELETED("강제 삭제됨");
     private final String PostStatus;
 }
