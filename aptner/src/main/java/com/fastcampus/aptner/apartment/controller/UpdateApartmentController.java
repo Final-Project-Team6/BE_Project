5e2f7f9712b2a54210b0d06ef94fdce3d221d862
LimDae94 <piay801@naver.com>
2024-05-21 21:03:56 +0900

    ♻️REF : 아파트 스웨거 변경하기
    아파트 스웨거 내용 변경

--- aptner/src/main/java/com/fastcampus/aptner/apartment/controller/UpdateApartmentController.java
+++ aptner/src/main/java/com/fastcampus/aptner/apartment/controller/UpdateApartmentController.java
@@ -5,7 +5,6 @@ import com.fastcampus.aptner.member.dto.HttpResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
