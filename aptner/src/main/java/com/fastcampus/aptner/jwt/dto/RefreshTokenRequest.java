ecf4721a0fb76e5f8f6133f8520f989044ab50f4
LimDae94 <piay801@naver.com>
2024-05-26 15:06:59 +0900

    ♻️REF : 리프레시 토큰 스웨거 수정하기
    ♻️REF : 리프레시 토큰 스웨거 수정하기

--- aptner/src/main/java/com/fastcampus/aptner/jwt/dto/RefreshTokenRequest.java
+++ aptner/src/main/java/com/fastcampus/aptner/jwt/dto/RefreshTokenRequest.java
@@ -1,9 +1,13 @@
 package com.fastcampus.aptner.jwt.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+@Schema(name = "리프레시 토큰 재발행 Body",description = "리프레시 토큰 재발행 요청 Body")
 @Data
 public class RefreshTokenRequest {
+
+    @Schema(description = "이전 리프레시 토큰 입력(필수)")
     String refreshToken;
 }
