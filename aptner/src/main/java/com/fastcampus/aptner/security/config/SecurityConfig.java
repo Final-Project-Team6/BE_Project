f92b0687a2ef3e3cbd0e28a56100dbe92ef90301
LimDae94 <piay801@naver.com>
2024-05-29 12:15:11 +0900

    ♻️REF : 시큐리티 경로 수정하기
    ♻️REF : 시큐리티 경로 수정하기

--- aptner/src/main/java/com/fastcampus/aptner/security/config/SecurityConfig.java
+++ aptner/src/main/java/com/fastcampus/aptner/security/config/SecurityConfig.java
@@ -81,11 +81,10 @@ public class SecurityConfig {
                                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll() // Preflight 요청은 허용
                                 .requestMatchers("/api/member/join", "/api/member/login", "/api/member/logout").permitAll()
                                 .requestMatchers(GET,"/api/post/**").permitAll() 
-                                .requestMatchers("/api/member/**", "/api/apartment/**", "/api/refresh-token/**").hasAnyRole("USER", "MANAGER", "ADMIN")
+                                .requestMatchers("/api/member/**", "/api/apartment/**", "/api/refresh-token/**", "/api/post/**").hasAnyRole("USER", "MANAGER", "ADMIN")
                                 .requestMatchers("/api/admin/**").hasAnyRole("MANAGER","ADMIN")
-                                .requestMatchers(HttpMethod.GET,"/api/post/**").permitAll()
-                                .requestMatchers("/api/post/**").hasAnyRole("USER", "MANAGER", "ADMIN")
-                                .requestMatchers("/api/refresh-token/**").hasAnyRole("USER", "MANAGER", "ADMIN")
+                                .requestMatchers("/api/post/communication/admin/", "/api/post/communication/category/admin/").hasAnyRole("MANAGER","ADMIN")
+                                .requestMatchers("/api/post/information/admin/", "/api/post/information/category/admin/" ).hasAnyRole("MANAGER","ADMIN")
                                 .requestMatchers("/swagger/**","/api-docs/**", "/v3/api-docs/**", "/swagger-resources/**", "/swagger-ui/**", "/webjars/**", "/").permitAll()
                                 .anyRequest().authenticated()
                 );
