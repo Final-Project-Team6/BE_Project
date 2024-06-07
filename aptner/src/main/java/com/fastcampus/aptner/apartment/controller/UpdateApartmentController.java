01f9b28bc6b5a47f3a8dba1320e54509977b27a0
LimDae94 <piay801@naver.com>
2024-06-07 10:06:35 +0900

    ✨ENH : 아파트 스웨거 정리하기
    ✨ENH : 아파트 스웨거 정리하기

--- aptner/src/main/java/com/fastcampus/aptner/apartment/controller/UpdateApartmentController.java
+++ aptner/src/main/java/com/fastcampus/aptner/apartment/controller/UpdateApartmentController.java
@@ -2,6 +2,7 @@ package com.fastcampus.aptner.apartment.controller;
 
 import com.fastcampus.aptner.apartment.service.UpdateApartmentService;
 import com.fastcampus.aptner.member.dto.HttpResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +12,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/apartment")
 @RestController
 public class UpdateApartmentController {
-    // TODO: 예외 및 유효성 검사 처리가 필요하다.
 
     private final UpdateApartmentService apartmentService;
 
-    @PostMapping("/update-name/{apartmentId}")
+    @Operation(
+            summary = "아파트 이름 수정 API\n\n",
+            description = "apartmentId: 아파트 고유 식별 번호\n\n" +
+                    "newName: 아파트 새로운 이름"
+    )
+    @PostMapping("/update/{apartmentId}")
     public ResponseEntity<?> updateApartmentName(@PathVariable Long apartmentId, @RequestBody String newName) {
         apartmentService.updateApartmentName(apartmentId, newName);
         return new ResponseEntity<>(new HttpResponse<>(1, "아파트 이름 업데이트 성공", null), HttpStatus.OK);
