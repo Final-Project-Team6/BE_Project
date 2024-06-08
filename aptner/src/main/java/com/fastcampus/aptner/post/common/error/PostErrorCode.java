2a87d76558c0ff944ddc2704265b9c73cdca8e84
leegyuseok <yigyuseok@gmail.com>
2024-06-08 16:09:12 +0900

    ✨ENH : 내 댓글 조회시 게시판별 조회 기능

--- aptner/src/main/java/com/fastcampus/aptner/post/common/error/PostErrorCode.java
+++ aptner/src/main/java/com/fastcampus/aptner/post/common/error/PostErrorCode.java
@@ -12,7 +12,8 @@ public enum PostErrorCode implements ErrorCode {
     NOT_ALLOWED_APARTMENT(HttpStatus.FORBIDDEN, "The apartment access is not permitted"),
     CANT_DELETE(HttpStatus.BAD_REQUEST, "Cant delete because it contains post"),
     NO_SUCH_POST(HttpStatus.NOT_FOUND,"The post matching the given ID could not be found"),
-    NO_SUCH_CATEGORY(HttpStatus.NOT_FOUND,"The Category matching the given ID could not be found");
+    NO_SUCH_CATEGORY(HttpStatus.NOT_FOUND,"The Category matching the given ID could not be found"),
+    COMMENT_CONVERT_FAILED(HttpStatus.INTERNAL_SERVER_ERROR,"error when converting comment");
 
     private final HttpStatus httpStatus;
     private final String message;
